196d8ea95677079af913baff5557bb2ba6ba436f
HaoPeiPei <18971573893@163.com>
2019-03-02 14:55:52 +0800

    register

--- pages/logIndex/logIndex.js
+++ pages/logIndex/logIndex.js
@@ -5,6 +5,8 @@ var imgRoot = app.globalData.imgRoot;
 var httpRequst = require("../../utils/requst.js");
 //联系手机正则
 var mobileReg = /^1(3[0-9]|4[57]|5[0-35-9]|8[0-9]|7[0-9])\d{8}$/;
+//验证码正则
+var regVerify = /^\d{6}$/;
 Page({
 
   /**
@@ -19,6 +21,11 @@ Page({
       background_url: imgRoot+"/images/login_bg.png"
     },
     imgRoot: imgRoot,
+    wait: 120,
+    waitStyle: 'c-bg-f4393c',
+    waitMsg: '发送验证码',
+    mobile: '',
+    verifyCode: '',
   },
   //返回
   catchBackChange: function (e) {
@@ -33,12 +40,76 @@ Page({
       phoneNumber: phoneNumber
     });
   },
+  //发送验证码
+  sendVerify(){
+    var that = this;
+    var mobile = this.data.mobile;
+    if (!this.checkMobile(mobile)) {
+      wx.showToast({
+        title: '请输入正确的手机号!',
+        icon: 'none',
+      });
+      return false;
+    }
+    if (this.data.wait == 120) {
+      that.countDown();
+      var params = {
+        action: "exists", 
+        mobile: mobile
+      };
+      wx.showLoading({
+        title: '加载中',
+      });
+      httpRequst.HttpRequst(true, "/weixin/miniprogram/ashx/login.ashx", params,"POST",function(res){
+        wx.hideLoading()
+        wx.showToast({
+          title: res.Message,
+          icon: 'none'
+        });
+        if (!res.Success) {
+          clearInterval(timer);
+          that.setData({
+            waitStyle: 'c-bg-f4393c',
+            waitMsg: '发送验证码',
+            wait: 120,
+          });
+        }
+      });
+    }
+  },
+  //倒计时
+  countDown() {
+    var that = this;
+    timer = setInterval(res=>{
+      if (that.data.wait == 0) {
+        that.setData({
+          waitStyle: 'c-bg-f4393c',
+          waitMsg: '发送验证码',
+          wait: 120,
+        });
+        clearInterval(timer);
+      } else {
+        that.setData({
+          waitStyle: 'c-bg-ccc',
+          waitMsg: `发送验证码(${that.data.wait--}s)`,
+          wait: that.data.wait--,
+        });
+      };
+    },1000)
+  },
+  //输入验证码，手机号
+  bindinput(e){
+    var id = e.currentTarget.id;
+    this.setData({
+      [id]: e.detail.value
+    })
+  },
   //登陆
   formSubmit:function(e){
     var _this= this;
     var mobile = e.detail.value.mobile;
-    var password = e.detail.value.password;
-    if(this.check(mobile, password)){
+    var verifyCode = e.detail.value.verifyCode;
+    if(this.check(mobile, verifyCode)){
       var params = {
         mobile: mobile,
         password: password,
@@ -59,7 +130,7 @@ Page({
     }
   },
   //验证
-  check(mobile, password) {
+  check(mobile, verifyCode) {
     if (!mobileReg.test(mobile)) {
       wx.showToast({
         title: '请输入正确的手机号码',
@@ -67,7 +138,7 @@ Page({
       });
       return false;
     }
-    if (password == "") {
+    if (regVerify.test(verifyCode)) {
       wx.showToast({
         title: '请输入密码',
         icon: 'none',
